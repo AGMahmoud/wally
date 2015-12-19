4e30049918fa50aa4861261ebda1020bf3d9e050
TigerDWong <buaaben@163.com>
2015-12-19 18:45:13 +0800

    committed automatically by script at  at 18:45

---
+++ Project/toolbox/src/routine.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+# routine.sh
+# Do some routines automatically
+# 1. git responsity
+# 2. email
+
+git_branch="master"		# git branch, default master
+
+ws_list=""
+
+git_push(){
+    path=$1			# path need to be absolute
+    cd $path
+    git add --ignore-removal .	# .gitignore considered to be well configured
+    git commit -m "committed automatically by script at $home at `date +%H:%M`"
+    git push origin master
+}
+
+ws="~/Wally"
+git_push $ws
+# git "~/.emacs.d"
+# git "~/Wally/TagerillWong/"
